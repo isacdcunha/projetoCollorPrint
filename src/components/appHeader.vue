d8e82ce0e864faa711529c5b60c247a0894664ea
isacdcunha <isabellecunhaifc@gmail.com>
2026-02-21 21:46:37 -0300

    feat: initial page almost finished

---
+++ src/components/appHeader.vue
@@ -0,0 +1,80 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <div class="header">
+    <img src="/images/logo collor print31_page-0001.jpg" alt="logo">
+
+    <ul>
+      <li><a href="">Home</a></li>
+      <li><a href="">Serviços</a></li>
+      <li><a href="">Portfólio</a></li>
+      <li><a href="">Contato</a></li>
+    </ul>
+  </div>
+</template>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@500&display=swap');
+
+*{
+    margin: 0;
+    padding: 0;
+    font-family: 'Poppins', sans-serif;
+}
+
+.header {
+  height: 120px;
+  display: flex;
+  align-items: center;
+  gap: 200px;          
+  padding-left: 100px;
+  background-color: white;
+  box-shadow: 0 4px 10px rgba(0, 0, 0, 0.1);
+}
+
+.header ul {
+  display: flex;
+  gap: 140px;
+  list-style: none;
+  margin: 0;
+  padding: 0;
+}
+
+.header img {
+  width: 200px;
+  height: 70px;
+  width: auto;
+  object-fit: contain;
+  display: block;
+}
+
+.header a{
+text-decoration: none;
+color: #1C1C1C;
+font-size: 17px;
+position: relative;
+display: inline-block;
+transition: 0.5s;
+}
+
+.header a::after {
+  content: '';
+  position: absolute;
+  width: 100%;
+  height: 2px;
+  background: linear-gradient(to left, #0a2c99, #f301aa);
+  bottom: -5px;
+  left: 0;
+  transform: scaleX(0);
+  transform-origin: right;
+  transition: transform 0.4s ease-out;
+}
+
+
+.header a:hover::after {
+  transform: scaleX(1);
+}
+
+</style>
---
+++ src/views/homeView.vue
@@ -0,0 +1,89 @@
+<script setup lang="ts">
+import  appHeader from "@/components/appHeader.vue";
+</script>
+
+<template>
+    <header>
+        <appHeader/>
+    </header>
+    <main>
+        <div class="left">
+            <h1><span>Comunicação</span> Visual</h1>
+            <p>Collor Print: Fazendo sua marca aparecer</p>
+            <button class="btn">Sobre nós</button>
+        </div>
+        <div class="right">
+            <img src="/public/images/freepik__talk__77966-removebg-preview.png">
+        </div>
+    </main>
+</template>
+
+<style scoped>
+*{
+    font-family: 'Poppins', sans-serif;
+}
+
+main{
+    background-color: #FEFBFE;
+    margin-top: 10px;
+    display: flex;
+    align-items: center;     
+    justify-content: space-between;
+    padding: 0 100px;
+    gap: 165px;
+    height: calc(100vh - 120px);
+}
+
+.btn{
+  padding: 12px 30px;
+  border: none;
+  border-radius: 30px;
+  color: white;
+  font-size: 16px;
+  cursor: pointer;
+  align-self: center;
+  margin-top: 20px;
+
+  background: linear-gradient(45deg, #0a2c99, #f301aa);
+  background-size: 200%;
+  transition: 0.5s;
+}
+
+.btn:hover{
+  background-position: right;
+}
+
+span{
+    color:#0a2c99;
+}
+
+.left{
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+}
+
+.left h1{
+    width: 200px;
+    font-size: 60px;
+    color: #da0098;
+    line-height: 1.1;
+    margin: 0;    
+}
+
+.left p{
+    width: 400px;
+    font-size: 18px;
+}
+
+.right{
+    display: flex;
+    justify-content:flex-end;
+    align-items: center;
+}
+
+.right img{
+    height: 500px;
+    width: 700px;
+}
+</style>
